dc5af4491b12327bc63ba9909529667cc275fa89
pratiksolanki09 <ps6535754@gmail.com>
2025-06-27 22:36:51 +0530

    Small fixes

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+from src import static
+from src import prompts
+from src import services
+from src import controller
+
+__version__ = "0.1.0"
+__author__ = "Animate AI"
+__all__ = ["static", "prompts", "services", "controller"]
